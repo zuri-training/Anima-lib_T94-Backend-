1374cbba50a0020d6dbddecf074581dbc9972cae
Nedu <nedumrobert@gmail.com>
2022-08-15 10:00:30 -0400

    fix: cors issue

--- src/app.js
+++ src/app.js
@@ -9,18 +9,18 @@ const NotFoundException = require("./shared/http-exceptions/not-found.exception"
 
 const app = express();
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept, Authorization, 'Content-Type' : 'multipart/form-data' ,* "
-//     );
-//     res.header(
-//         "Access-Control-Allow-Methods",
-//         "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-//     );
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization, 'Content-Type' : 'multipart/form-data' ,* "
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
+    next();
+});
 
 // app.use(cors("*"));
 app.use(cors())
