ec5b6972aeccfee23f505a5792502825996c571a
Nedu <nedumrobert@gmail.com>
2022-08-15 09:51:17 -0400

    fix: fixed cors issue

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const cors = require("cors");
 const express = require("express");
+const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const routes = require("./routes/routes");
@@ -9,26 +9,21 @@ const NotFoundException = require("./shared/http-exceptions/not-found.exception"
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization, 'Content-Type' : 'multipart/form-data' ,* "
-    );
-    res.header(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-    );
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept, Authorization, 'Content-Type' : 'multipart/form-data' ,* "
+//     );
+//     res.header(
+//         "Access-Control-Allow-Methods",
+//         "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+//     );
+//     next();
+// });
 
 // app.use(cors("*"));
-app.use(cors(
-    {
-    origin: "http://localhost:3000",
-    credentials: true,
-}
-))
+app.use(cors())
 app.use(helmet());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
