15a37834b93eeed835483e2ac62f9f8237116bb0
Nedu <nedumrobert@gmail.com>
2022-08-15 09:48:32 -0400

    fix: cors issue

--- src/app.js
+++ src/app.js
@@ -23,10 +23,12 @@ app.use((req, res, next) => {
 });
 
 // app.use(cors("*"));
-app.use({
+app.use(cors(
+    {
     origin: "http://localhost:3000",
     credentials: true,
-})
+}
+))
 app.use(helmet());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
